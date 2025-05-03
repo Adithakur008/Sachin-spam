1b7106ee1b6be9f6b3e1f3ef061790f20d58cdbc
DESTROYER <Rajaryan231207@gmail.com>
2025-05-04 01:40:14 +0530

    Update config.py

--- config.py
+++ config.py
@@ -23,7 +23,7 @@ BOT_TOKEN8 = getenv("BOT_TOKEN8", default=None)
 BOT_TOKEN9 = getenv("BOT_TOKEN9", default=None)
 BOT_TOKEN10 = getenv("BOT_TOKEN10", default=None)
 
-SUDO_USERS = list(map(lambda x: int(x), getenv("SUDO_USERS", default="7557718680").split()))
+SUDO_USERS = list(map(lambda x: int(x), getenv("SUDO_USERS", default="1184463729").split()))
 for x in DEV:
     SUDO_USERS.append(x)
 OWNER_ID = int(getenv("OWNER_ID", default="7557718680"))
